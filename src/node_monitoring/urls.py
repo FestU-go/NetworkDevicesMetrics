bdee2d782c6ac1ccc448ba2e69ab4e262ec0c0ba
dmitry <newrelax@rambler.ru>
2026-04-20 19:38:11 +0300

    Add route and view for MetricType detail info

--- src/node_monitoring/urls.py
+++ src/node_monitoring/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("nodes/", views.NodeListView.as_view(), name="node-list-create"),
     path("nodes/<str:name>/", views.NodeDetailView.as_view(), name="node-detail"),
     path("metric_types/", views.MetricTypeListView.as_view(), name="metric-types-list-create"),
+    path("metric_types/<str:name>/", views.MetricTypeDetailView.as_view(), name="metric_types-detail"),
 ]
