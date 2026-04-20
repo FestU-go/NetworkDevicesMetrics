13a5350a44e0eaf2e534b194ef9649ef9dc260c1
dmitry <newrelax@rambler.ru>
2026-04-20 19:37:43 +0300

    Add MetricType serializer

--- src/node_monitoring/serializers.py
+++ src/node_monitoring/serializers.py
@@ -1,22 +1,29 @@
 from rest_framework import serializers
 from node_monitoring.models import Node, MetricType
 
-class NodeSerializer(serializers.ModelSerializer):
+class NodeDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Node
         fields = ['id', 'name', 'expected_os_name', 'expected_cpu_name', 'max_cpu_load', 'min_free_disk_gb']
         read_only_fields = ['id']
 
-class NodeListSerializer(NodeSerializer):
+class NodeListSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="node-detail", read_only=True, lookup_field="name", lookup_url_kwarg="name")
-
     class Meta:
         model = Node
         fields = ['url', 'id', 'name', 'expected_os_name', 'expected_cpu_name', 'max_cpu_load', 'min_free_disk_gb']
         read_only_fields = ['id']
 
-class MetricTypeListSerializer(serializers.ModelSerializer):
+class MetricTypeDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = MetricType
         fields = ['id', 'name', 'display_name', 'collect_interval_minutes',]
         read_only_fields = ['id']
+
+class MetricTypeListSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="metric_types-detail", read_only=True, lookup_field="name", lookup_url_kwarg="name")
+    class Meta:
+        model = MetricType
+        fields = ['url', 'id', 'name', 'display_name', 'collect_interval_minutes',]
+        read_only_fields = ['id']
+        
