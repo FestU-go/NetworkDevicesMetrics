7415cc1400c52c3a670cf96d121099df4895e39d
dmitry <newrelax@rambler.ru>
2026-04-21 16:56:31 +0300

    Add drf_spectacular to the project

--- src/networkdevicesmetrics/urls.py
+++ src/networkdevicesmetrics/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("su/", admin.site.urls),
     path("api/", include("node_monitoring.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
